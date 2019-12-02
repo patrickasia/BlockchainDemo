d880db772b7d0ceaa152e13459c8554350ebf7d8
Patrick <patricklong1680@gmail.com>
2019-12-03 07:49:52 +0900

    Update dynamicdns.js

--- dynamicdns.js
+++ dynamicdns.js
@@ -1,9 +1,3 @@
-/**
- * MIT License
- * Copyright (c) 2018 Patrick (p@trick.asia)
- * https://github.com/patrickasia/BlockchainDemo/blob/master/LICENSE
- */
-
 const http = require('http')
 const qs = require('querystring')
 const request = require('request')
